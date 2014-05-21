7b4bdee2c900790da6031d1b046f55e2482ff3ce
schoolie <brian.p.schoolcraft@gmail.com>
2014-05-20 22:17:11 -0400

    ajax request working

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 // Here's my data model
 var ViewModel = function(first, last) {
+    self = this;
     this.firstName = ko.observable(first);
     this.lastName = ko.observable(last);
  
     this.fullName = ko.computed(function() {
         // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
         return this.firstName() + " " + this.lastName();
-    }, this);
+    }, this);    
     
+    this.points = ko.observableArray([]);
+        
+    this.requestData = function() {
+      $.ajax({
+            url: "http://localhost:5000/api/v1.0/courses/1",
+            type: "GET",
+            dataType: "jsonp",
+            success: function(allData) {
+              var points = allData.data.mapPoints;
+              var mappedPoints = $.map(points, function(item) { return new Point(item) });
+              self.points(mappedPoints);
+            }
+        }); 
+    }
     
-    this.months =  [ new Button(1), new Button(2) ];
+    this.requestData();
 }
  
-var Button = function(num) {
-    this.label = "CP"+num;
+var Point = function(data) {
+    this.id = ko.observable(data.id)
+    this.lat = ko.observable(data.lat)
+    this.lon = ko.observable(data.lon)
+    this.title = ko.observable(data.title)
+    this.type = ko.observable(data.type)
+    this.lat = ko.observable(data.lat)
 }
 ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+
+
