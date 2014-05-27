be1e733780506317b2ef1fa8a0706f6f6fbd4554
schoolie <brian.p.schoolcraft@gmail.com>
2014-05-27 12:26:40 -0400

    location updating

--- js/main.js
+++ js/main.js
@@ -26,10 +26,10 @@ var ViewModel = function(first, last) {
     
     this.requestData();
     
-    self.location = ko.observable();
+    this.locationString = ko.observable();
     
     var onSuccess = function(position) {
-        alert('Latitude: '          + position.coords.latitude          + '\n' +
+        self.locationString('Latitude: '          + position.coords.latitude          + '\n' +
               'Longitude: '         + position.coords.longitude         + '\n' +
               'Altitude: '          + position.coords.altitude          + '\n' +
               'Accuracy: '          + position.coords.accuracy          + '\n' +
@@ -37,16 +37,17 @@ var ViewModel = function(first, last) {
               'Heading: '           + position.coords.heading           + '\n' +
               'Speed: '             + position.coords.speed             + '\n' +
               'Timestamp: '         + position.timestamp                + '\n');
+        
     };
 
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-              'message: ' + error.message + '\n');
+        self.location = 'code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n';
     }
 
-    document.addEventListener("deviceready", function() {navigator.geolocation.getCurrentPosition(onSuccess, onError);}, false);
+    document.addEventListener("deviceready", function() {navigator.geolocation.watchPosition(onSuccess, onError, { frequency: 3000, enableHighAccuracy: true });}, false);
             
 }
  
